3721b45b9575e6b3fc1196fe3b91fdecd402ea4c
Jo-annRemmelink <jo-ann16@live.nl>
2016-03-17 08:59:33 +0100

    C05
    image map toegevoegd

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="index.css" type="text/css" rel="stylesheet">
+<title>Untitled Document</title>
+    <img src="Images/webshop-foto.jpg" alt=""/>
+    <h1 class="logo">
+        <img src="Images/LogoWebshop.png" alt=""/>
+    </h1>
+</head>
+<body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
