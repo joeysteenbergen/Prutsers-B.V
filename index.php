fa49c8b9015e0adb0905ce2e5312a189858d74d1
Jo-annRemmelink <jo-ann16@live.nl>
2016-03-21 09:14:38 +0100

    C10
    Add Bootstrap

--- index.php
+++ index.php
@@ -2,23 +2,25 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="Bootstrap/bootstrap-3.3.6-dist/bootstrap-3.3.6-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link href="index.css" type="text/css" rel="stylesheet">
 <title>Untitled Document</title>
 
 </head>
 <body>
 <div>
-    <header class="head">
-        <h1 class="logo">
+    <div class=""
+    <header class="">
+        <h1 class="">
             <img class="headerImgage" src="Images/LogoWebshop2.png" alt=""/>
         </h1>
     </header>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-<a href="gebruiker/LoginPage.php">Login</a><br/>
-<a href="gebruiker/RegisterPage.php">Register</a>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>
