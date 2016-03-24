938501d5d77ff6aa62e11fa10080a417ded4139f
Jo-annRemmelink <jo-ann16@live.nl>
2016-03-24 08:53:56 +0100

    C11
    menu's

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="Bootstrap/bootstrap-3.3.6-dist/bootstrap-3.3.6-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link href="index.css" type="text/css" rel="stylesheet">
@@ -9,13 +10,35 @@
 
 </head>
 <body>
-<div>
-    <div class=""
-    <header class="">
-        <h1 class="">
+<div class="container-fluid">
+    <div class="row">
+        <header class="col-md-12 head">
             <img class="headerImgage" src="Images/LogoWebshop2.png" alt=""/>
-        </h1>
-    </header>
+        <!-- <h1 class="rotate">Wij verkopen rotzooi!</h1>-->
+        </header>
+    </div>
+    <div class="row">
+        <ul class="nav nav-pills menuExtra">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Profile</a></li>
+            <li role="presentation"><a href="#">Messages</a></li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-md-2 strech">
+            <ul id="sidebar" class="nav nav-pills nav-stacked>
+        <h1 class="">
+            <ul class="nav nav-pills nav-stacked">
+                <li class="active"><a href="#">Catogorieën</a></li>
+                <li><a href="#">Menu 1</a></li>
+                <li><a href="#">Menu 2</a></li>
+                <li><a href="#">Menu 3</a></li>
+            </ul>
+        </div>
+    <div class="col-md-10 strech">
+
+    </div>
+    </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
